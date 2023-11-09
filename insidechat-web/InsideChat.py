74fcb784afe026225d5a6e6e0b64a5c34280f312
ortizalfano <42744183+ortizalfano@users.noreply.github.com>
2023-11-09 12:25:01 +0100

    Update InsideChat.py

--- insidechat-web/InsideChat.py
+++ insidechat-web/InsideChat.py
@@ -120,11 +120,11 @@ def main():
 if __name__ == '__main__':
     main()
 
-#hide_st_style = """
-#            <style>
-#            #MainMenu {visibility: hidden;}
-#            footer {visibility: hidden;}
-#            header {visibility: hidden;}
-#            </style>
-#           """
-#st.markdown(hide_st_style, unsafe_allow_html=True)
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+           """
+st.markdown(hide_st_style, unsafe_allow_html=True)
